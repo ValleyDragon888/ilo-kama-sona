ee303b52bc0512b9a1d3ed8ffd213255b19af9bc
ValleyDragon888 <ValleyDragon888@gmail.com>
2025-01-20 16:39:55 +0000

    add code to manipulate card value

---
+++ scripts/card.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+enum FlashcardType { LATIN, SITELEN, BOTH }
+@onready var latincard = $VBoxContainer/LatinCard
+@onready var sitelenponacard = $VBoxContainer/SitelenPonaCard
+@onready var sitelenlatincard = $VBoxContainer/SitelenLatinCard
+
+@export var word = "toki"
+@export var type: FlashcardType = FlashcardType.BOTH
+
+func _ready() -> void:
+	refresh_display()
+	
+func _process(delta: float) -> void:
+	pass
+
+func refresh_display():
+	set_flashcard_type(type)
+	match type:
+		FlashcardType.LATIN:
+			$VBoxContainer/LatinCard/Text.text = word
+		FlashcardType.SITELEN:
+			$VBoxContainer/SitelenPonaCard/SitelenPona.text = word
+		FlashcardType.BOTH:
+			$VBoxContainer/SitelenLatinCard/Text.text = word
+			$VBoxContainer/SitelenLatinCard/SitelenPona.text = word
+
+func set_flashcard_type(type: FlashcardType):
+	latincard.hide(); sitelenponacard.hide(); sitelenlatincard.hide
+	match type:
+		FlashcardType.LATIN: latincard.show()
+		FlashcardType.SITELEN: sitelenponacard.show()
+		FlashcardType.BOTH: sitelenlatincard.show()
