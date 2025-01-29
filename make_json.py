009687872b9f54c8cd1eefb9e896465dd6d5eead
ValleyDragon888 <ValleyDragon888@gmail.com>
2025-01-29 07:03:23 +0000

    add tooling for importing yaml > json

---
+++ make_json.py
@@ -0,0 +1,9 @@
+import json
+
+import yaml
+
+with open("flashcards.yaml", "r") as f:
+    contents = yaml.safe_load(f)
+
+with open("/project/assets/flashcards.json", "w+") as f:
+    f.write(json.dumps(contents, indent=4))
