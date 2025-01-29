0981a3b12a5ce1ebcbed825fac01165d3ca6263c
ValleyDragon888 <ValleyDragon888@gmail.com>
2025-01-29 07:06:59 +0000

    make python script work

--- make_json.py
+++ make_json.py
@@ -5,5 +5,5 @@ import yaml
 with open("flashcards.yaml", "r") as f:
     contents = yaml.safe_load(f)
 
-with open("/project/assets/flashcards.json", "w+") as f:
+with open("project/assets/flashcards.json", "w+") as f:
     f.write(json.dumps(contents, indent=4))
