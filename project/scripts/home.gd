0d765fb14d95252da4fd2e5e19aa155c2953f84e
ValleyDragon888 <ValleyDragon888@gmail.com>
2025-01-29 16:42:10 +0000

    make menu work

--- project/scripts/home.gd
+++ project/scripts/home.gd
@@ -2,9 +2,14 @@ extends CanvasLayer
 
 var courses = ["pu", "toki pona app"]
 var selected = 0
+@onready var lessons = preload("res://assets/flashcards.json")
+var current_lessons
+var lesson_selected = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	lessons = lessons.get_data()
+	current_lessons = lessons["pu"]
 	update_course_showing()
 
 func _process(delta: float) -> void:
@@ -16,6 +21,14 @@ func update_course_showing():
 		$VBoxContainer/HBoxContainer/CourseIndicator.add_theme_font_override("font", load("res://assets/fonts/nasin-nanpa-4.0.1.otf"))
 	else:
 		$VBoxContainer/HBoxContainer/CourseIndicator.remove_theme_font_override("font")
+		
+	current_lessons = lessons[courses[selected]]
+	$VBoxContainer/LessonSelect.get_popup().clear()
+	for i in range(len(current_lessons)):
+		$VBoxContainer/LessonSelect.get_popup().add_item(current_lessons.keys()[i], i)
+	
+	$VBoxContainer/LessonSelect.get_popup().id_pressed.connect(_on_new_lesson_selected)
+	
 
 func _on_kama_pressed() -> void:
 	selected -= 1
@@ -26,3 +39,11 @@ func _on_tawa_pressed() -> void:
 	selected += 1
 	if selected >= len(courses): selected = 0
 	update_course_showing()
+
+func _on_lesson_select_pressed() -> void:
+	pass
+
+func _on_new_lesson_selected(id: int):
+	print(id)
+	lesson_selected = id
+	$VBoxContainer/LessonSelect.text = current_lessons.keys()[id]
