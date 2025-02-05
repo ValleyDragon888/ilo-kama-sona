1ab6a23d1b3bb7171ebf2107f20d7e71b9b64e6f
ValleyDragon888 <ValleyDragon888@gmail.com>
2025-02-05 06:54:53 +0000

    add toki pona difinitions file and move tokipona flashcards

--- make_json.py
+++ make_json.py
@@ -2,7 +2,7 @@ import json
 
 import yaml
 
-with open("flashcards.yaml", "r") as f:
+with open("flashcard-source/tokipona-flashcards.yaml", "r") as f:
     contents = yaml.safe_load(f)
 
 with open("project/assets/flashcards.json", "w+") as f:
