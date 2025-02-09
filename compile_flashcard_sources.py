af772caafb516b94282b1fc9fc94b840a983970f
ValleyDragon888 <ValleyDragon888@gmail.com>
2025-02-09 08:12:43 +0000

    lesson names now no longer have dashes in their names

--- compile_flashcard_sources.py
+++ compile_flashcard_sources.py
@@ -2,9 +2,19 @@ import json
 import yaml
 import csv
 
+def remove_space_placeholder(lessons):
+    new_lessons = {}
+    for course in lessons.keys():
+        new_lessons[course] = {}
+        for lesson_name in lessons[course].keys():
+            new_lesson_name = lesson_name.replace("-", " ")
+            new_lessons[course][new_lesson_name] = lessons[course][lesson_name]
+    return new_lessons
+
 ## tokipona-flashcards.yaml ##
 with open("flashcard-source/tokipona-flashcards.yaml", "r") as f:
     contents = yaml.safe_load(f)
+contents = remove_space_placeholder(contents)
 
 with open("project/assets/tokipona-flashcards.json", "w+") as f:
     f.write(json.dumps(contents, indent=4))
